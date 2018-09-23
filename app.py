668cc4fe0367c763a270716c0b836a8add789c90
Gabriela Voicu <gabrielavoicu16@gmail.com>
2018-09-23 12:30:29 -0500

    refactor and docstrings
    refactor away the reading of the usernames
    add dostrings for the class and two methods

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import requests
+
 from flask import Flask
 from flask_restful import reqparse, Resource, Api
 
@@ -5,16 +7,40 @@ app = Flask(__name__)
 api = Api(app)
 
 class GitProfile(Resource):
+
+    """Return a merged git profile for a user.
+
+    Given a valid BitBucket and a valid GitHub usernames, the class will return a merged profile
+    of the user in JSON format.
+    """
+
     def get(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('github_username', type=str, help='GitHub username needs to be a string value.')
-        parser.add_argument('bitbucket_username', type=str, help='BitBucket username needs to be a string value.')
-        args = parser.parse_args()
-        github_username = args.get('github_username', None)
-        bitbucket_username = args.get('bitbucket_username', None)
+        """Return a merged git profile provided two valid git usernames for GitHub and BitBucket.
+
+        This method orchestrates all the steps needed to create the merged profile: read the usernames
+        from the query string, validate the usernames, call GitHub and BitBucket for the relevant information
+        for each username and return a JSON of the merged profile.
+        """
+        github_username, bitbucket_username = self.get_usernames()
         if (github_username == None) or (bitbucket_username == None):
             return { 'message': 'A merged user profile could not be returned. Please provide a github_username and a bitbucket_username key.'}, 400
-        return args
+        github_response = requests.get('https://api.github.com/users/gabivoicu')
+        bitbucket_response = requests.get('https://api.bitbucket.org/1.0/users/muntzen')
+        return bitbucket_response.json()
+
+    def get_usernames(self):
+        """Get the git usernames.
+
+        Read the git usernames from the query string. If one or more of the usernames if not
+        provided, return None for its value.
+        """
+        parser = reqparse.RequestParser()
+        parser.add_argument('github_username')
+        parser.add_argument('bitbucket_username')
+        args = parser.parse_args()
+        github = args.get('github_username', None)
+        bitbucket = args.get('bitbucket_username', None)
+        return github, bitbucket
 
 api.add_resource(GitProfile, '/git_profile/')
 
